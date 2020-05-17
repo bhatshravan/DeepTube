ac51ef7bfbc558def6a6510cef0bae68200f4862
Shravan Bhat <shrvnbhat0@gmail.com>
2020-05-17 23:56:41 +0530

    Add download

--- downloadChannel.py
+++ downloadChannel.py
@@ -2,6 +2,7 @@ import pafy
 import urllib
 import requests
 import json
+from os import path
 import os
 
 # Get api key from .env
@@ -9,7 +10,7 @@ import os
 from dotenv import load_dotenv
 project_folder = os.path.expanduser('./')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
-api_key = os.getenv('api_key3')
+api_key = os.getenv('api_key5')
 
 # Api key test
 def getUrlTest(channel_id):
@@ -25,25 +26,20 @@ def getUrlTest(channel_id):
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
     first_url = base_search_url + \
-        'key={}&channelId={}&part=snippet&maxResults=50&order=viewCount&regionCode=US'.format(
+        'key={}&channelId={}&part=snippet&maxResults=5&order=viewCount&regionCode=US'.format(
             api_key, channel_id)
     print(first_url)
+
 
 # Retrieve list of all videos in channel
 def get_all_video_in_channel(channel_id):
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
-    if not channel_id.startswith("UC"):
-        u_url = "https://www.googleapis.com/youtube/v3/channels?key={}&forUsername={}&part=id".format(
-            api_key, channel_id)
 
-        inp = requests.get(u_url)
-        resp = inp.json()
-        channel_id = (resp["items"][0]["id"])
 
     first_url = base_search_url + \
-        'key={}&channelId={}&part=snippet&order=viewCount&maxResults=5&regionCode=US'.format(
+        'key={}&channelId={}&part=snippet&order=viewCount&maxResults=50&regionCode=US'.format(
             api_key, channel_id)
 
     video_links = []
@@ -56,9 +52,9 @@ def get_all_video_in_channel(channel_id):
         inp = requests.get(url)
         resp = inp.json()
         vids = []
-        if(pages>0):
+        if(pages>1):
             break
-        pages=pages+1
+        pages += 1
         try:
             for i in resp['items']:
                 if i['id']['kind'] == "youtube#video":
@@ -69,28 +65,23 @@ def get_all_video_in_channel(channel_id):
 
             mid = vids[int(len(vids)/2)]
             last = vids[-1]
-            # print(mid, last)
-
-            # vid_url = "https://www.googleapis.com/youtube/v3/videos?part=statistics&id={}&regionCode=US&key={}".format(
-            #     last, api_key)
-            # inp = requests.get(vid_url).json()
-            # print(inp["items"][0]["statistics"]["viewCount"])
 
             try:
                 next_page_token = resp['nextPageToken']
                 url = first_url + '&pageToken={}'.format(next_page_token)
             except:
                 break
+
         except Exception as e:
             print("API KEY ERRORRRR: ",e)
             break
-    return video_links, video_id
+
+    return channel_id,video_links, video_id
 
 # Use pafy to get video info
 def getVideoInfo(video,videoDate):
     try:
-        videoPaf = pafy.new(urls)
-        print(videoPaf.title,videoPaf.duration,videoPaf.likes,videoPaf.dislikes,videoPaf.viewcount,videoPaf.thumb,videoDate)
+        videoPaf = pafy.new(video)
         return videoPaf
     except Exception as e:
         print("ERROR ===== ",e)
@@ -99,22 +90,61 @@ def getVideoInfo(video,videoDate):
 
 def checkAvailable(videoId):
     r = requests.get("https://img.youtube.com/vi/"+videoId+"/0.jpg")
-    # print(r.status_code)
     if(r.status_code>400):
         return False
     return True
 
+#done = zefrank1, zach king, billwurtz, cgpgrey, 
+def main():
+    # channels = ["billwurtz","UC3Liv5-jdU2Qk53lTtYDG2w","zefrank1", "ZachKingVine","CorridorDigital","Airforceproud95","CaptainDisillusion","coldfustion","Computerphile","CinemaSins","UCdC0An4ZPNr_YiFiYoVbwaw","UCmh5gdwCx6lN7gEC20leNVA","corycotton","ErasableNinja","UCR1D15p_vdP3HkrH8wgjQRw","jacksfilms","UCYLS9TSah19IsB8yyUpiDzg","UCsXVk37bltHxD1rDPwtNM8Q","UCm9K6rby98W8JigLoZOh6FQ","UCfb2YpWR9FWTJMjzvAlP0_Q","UCY1kMZp36IQSyNx_9h4mpCg","UCBJycsmduvYEL83R_U4JriQ","UCY3TJECrA90t9YTrxhdjcVw","UCtHaxi4GTYDpJgMSGy7AeSw","UCX6OQ3DkcsbYNE6H8uQQuVA","UCxsQFG_8Dbt1sZhLReL2mUw","UCJkMlOu7faDgqh4PfzbpLdg","UCSAUGyc_xA8uYzaIVG6MESQ","UCNIuvl7V8zACPpTmmNIqP2A","UCeE3lj6pLX_gCd0Yvns517Q","UC7yF9tV4xWEMZkel7q8La_w","UC6MFZAOHXlKK1FI7V0XQVeA","Airforceproud95","UCAL3JXZSzSm8AlZyD3nQdBA","TheInfographicsShow","UCdN4aXTrHAtfgbVG9HjBmxQ","UCY3TJECrA90t9YTrxhdjcVw","UCfoK9LI9vmQQ36zqsFZtNJQ","UCvQECJukTDE2i6aCoMnS-Vg","UCq6aw03lNILzV96UvEAASfQ","UCEOXxzW2vU0P-0THehuIIeg","UC2C_jShtL725hvbm1arSV9w","UCYUQQgogVeQY8cMQamhHJcg","UC4QZ_LsYcvcq7qOsOhpAX4A","UC9-y-6csu5WGm29I7JiwpnA","UCsn6cjffsvyOZCZxvGoJxGg","UCn1XB-jvmd9fXMzhiA6IR0w","UCgrpA6pVkSU3WfCjL1NN7Ig","UCPcFg7aBbaVzXoIKSNqwaww","UCY1kMZp36IQSyNx_9h4mpCg","UCq6VFHwMzcMXbuKyG7SQYIg","UCmheCYT4HlbFi943lpH009Q","UC7OIF66iuk4yWXNOGluqEXA","UCfdNM3NAhaBOXCafH7krzrA","UCRcgy6GzDeccI7dkbbBna3Q","UCq8DICunczvLuJJq414110A","UCVpankR4HtoAVtYnFDUieYA"]
+    
+    channels = []
 
+    for channel in channels:
+        channelName = channel
 
-# lists = get_all_video_in_channel("UC-lHJZR3Gqxm24_Vd_AJ5Yw")
-# video_lists, video_id = get_all_video_in_channel("CGPGrey")
-# print(video_lists)
-# print(video_id)
-# video_id = ['iHzzSao6ypE', 'rNu8XDBSn10', '7Pq-S557XQU', 'qMkYlIA7mgw', 'kF8I_r9XT7A', 'jNgP6d9HraI', 'rStL7niR7gs', 'uqH_Y1TupoQ', 'ASSOQDQvVLU', 'eE_IUPInEuc', 'O37yJBFRrfg', 'sYzfKiIWN4g', 'wfYbgdo8e-8', 'YxgsxaFWWHQ', 'SCzXZfNIu3A', 'OPHRIjI3hXs', 'wOmjnioNulo', 'rE3j_RHkqJc', 'O2hO4_UEe-4', 'JEYh5WACqEk', '3uBcq1x7P34', 'oAHbLRjF0vo', 'OTVE5iPMKLg', 'y5UT04p5f7U', 'BUY6HGqYweQ', '4AivEQmfPpk', 'WKU0qDpu3AM', 'cZYNADOHhVY', 'KIbkoop4AYE', '84aWtseb2-4', 'LrObZ_HZZUc', 'tk862BbjWx4', 'Erp8IAUouus', 'S92fTz_-kQE', 'kcc_KAhwpa0', 'bhyYgnhhKFw', 'LO1mTELoj6o', 'C25qzDhGLx8', 'tlI022aUWQQ', 'Vui-qGCfXuA', 'piEayQ0T-qA', 's7tWHJfhiyo', 'm3_I2rfApYk', 'R9OHn5ZF4Uo', 'Z_2gbGXzFbs', 'SgZ1f4ACZBQ', 'LruaD7XhQ50', 'r9rGX91rq5I', 'F9-iSl_eg5U', '7wC42HgLA4k', 'DbKNlFcg02c', 'h3ppbbYXMxE', 'TsXMe8H6iyc', 'PM79Epw_cp8', 'ULBCuHIpNgU', 'naDCCW5TSpU', 'nQHBAdShgYI', 'xX96xng7sAE', 'l8XOZJkozfI', 'KW0eUrUiyxo', 'sHEDXzOfENI', 'z1ROpIKZe-c', 'ig_qpNfXHIU', '3Y3jE3B8HsE', 'Mky11UJb9AY', 'agZ0xISi40E', 'OUS9mM8Xbbw', 'p3HnMLq8m9U', 'J1Yv24cM2os', 'RbUVKXdu4lQ', 'VPBH1eW28mo', '7-Nl4JFDLOU', 'nU4E6SSy5Yg', 'G3wLQz-LgrM', 'Ex74x_gqTU0', '0dU4IMex4FU', 'hsWr_JWTZss', '_naDg-guomA', '_95I_1rZiIs', 'tUX-frlNBJY', 'wtt2aSV8wdw', 'utDHcbiOfKY', 'SumDHcnCRuU', 'orybDrUj4vA', 'QC-cMv0e3Dc', 'K63ZKa_tt3s', 'QT0I-sdoSXU', 'gaQwC5QbLeQ', 'WVZQapdkwLo', 'tlsU_YT9n_g', 'DIssymQvrbU', 'kh88fVP2FWQ', 'GOiIxqcbzyM', '7vsCAM17O-M', 'zcZTTB10_Vo', '0JK2dR8ei5E', 'jlLUuX2a0Cg', 'e-ZpsxnmmbE', 'snAhsXyO3Ck', 'NVGuFdX5guE', 'wpYtLcRKuPk', 'AM1-ecnQsm4', 'f-mHLBD64HM', 'wvWpdrfoEv0', 'NVmnLaTXylc', '3e5Jn2gG8Eg', 'QMNGEY8OZqo', 'U6ViKuXd5qQ', '8qGCAE1jte8', 'aN44ETT6mUY', 'swx6VyiJ7TI', 'Ac9070OIMUg', 'VvaxkpeSg_4', 'ZmD9RkCJgDI', 'rbHQObTeLDM', 'LIS0IFmbZaI', 'l_wFE9kHSos', 'SuhGwaZiNIk', 'aHRTtA7yTZ0', '6JN4RI7nkes', 'PukSDm0RD2E', 'w5UnNpnGSNI', 'jky6UhxCzTE', 'Pyv1A8StJSs', 'S6QmhrtBTO0', 'uR2DfpjIuXo', 'ABMV4wXx6Xo', 'wRc630BSTIg', 'kUS9uvYyn3A', '8DNtsjB7L_I', 'jxTaj_EfE9g', 'UEX16lWliQc', 'AqwJXTyfNqU', 'btDMr-13d3g', '8CQ3qWgH4B0', '5hPsX3zQeX4', 'uBcGdf4OjQw', 'hvt5xjZTN1U', 'iOWC0g_1IxQ', 'ZtOdVjF_73I', 'YVJXRq7IfgQ', 'ugcV7JSXFN0', 'jIQmdKmtxC4', 'XQuo4-t0p_8', 'KRzQVD6LfCE', 'YJxKvQ4R1GQ', 'nNgzAQINDtw', 'iyoB5erM-W8']
-video_id = ['iHzzSao6ypE,2016-08-31T12:36:39Z', 'rNu8XDBSn10,2011-01-30T16:19:41Z', '7Pq-S557XQU,2014-08-13T12:00:03Z', 'qMkYlIA7mgw,2013-06-05T21:35:43Z']
-for video in video_id:
-    video = video.split(",")
-    urls = "https://www.youtube.com/watch?v={}".format(video[0])
-    videoPaf = getVideoInfo(urls,video[1])
-    # if(not videoPaf):
-    #     print(video,videoPaf)
+        if not channel.startswith("UC"):
+            u_url = "https://www.googleapis.com/youtube/v3/channels?key={}&forUsername={}&part=id".format(
+                api_key, channel)
+
+            inp = requests.get(u_url)
+            resp = inp.json()
+            channel = (resp["items"][0]["id"])
+
+        raw_file_name = "./data/"+channel+".csv"
+        raw_file_name = raw_file_name.replace("_","----")
+        
+        if path.exists(raw_file_name):
+            continue
+
+        output_file = open(raw_file_name,"a")
+        output_data = ""
+
+        channel_id,video_lists, video_id = get_all_video_in_channel(channel)
+        
+        for video in video_id:
+            try:
+                video = video.split(",")
+                urls = "https://www.youtube.com/watch?v={}".format(video[0])
+
+                videoPaf = getVideoInfo(urls,video[1])
+
+                # if videoPaf.viewcount<100000:
+                #     break
+
+                if(not videoPaf=="Error"):
+                    videoMeta = [str(channel_id),str(video[0]),videoPaf.title.replace(",","---"),str(videoPaf.author),str(videoPaf.duration),str(videoPaf.likes),str(videoPaf.dislikes),str(videoPaf.viewcount),str(videoPaf.thumb),video[1]]
+                    
+                    videoStr = ",".join(videoMeta)
+                    videoStr = videoStr+"\n"
+                    output_file.write(videoStr)
+
+                    output_data += videoStr
+                    print(videoStr)
+            except:
+                continue
+
+        output_file.close()
+
+if __name__ == "__main__":
+    main()
